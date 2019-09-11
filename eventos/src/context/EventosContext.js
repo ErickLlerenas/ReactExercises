0ad7f07a7ea2aba78a7c36fca1db55a2108fb26a
Erick Llerenas <dev.llerenas@gmail.com>
2019-09-11 00:40:10 -0500

    Added Eventos API

---
+++ eventos/src/context/EventosContext.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+const EventosContext = React.createContext();
+export const EventosConsumer = EventosContext.Consumer;
+
+class EventosProvider extends Component {
+  token = "7KMEQOSF5OGY2VBTNRUB";
+  ordenar = "date";
+  state = {
+    eventos: []
+  };
+
+  obtenerEventos = async busqueda => {
+    const url = `https://www.eventbriteapi.com/v3/events/search/?q=${busqueda.nombre}&categories=${busqueda.categoria}&sort_by=${this.ordenar}&token=${this.token}&locale=es_ES`;
+
+    //Consultar la API con la URL
+    const eventos = await axios.get(url);
+    this.setState({
+        eventos : eventos.data.events
+    })
+  };
+  render() {
+    return (
+      <EventosContext.Provider
+        value={{
+          eventos: this.state.eventos,
+          obtenerEventos: this.obtenerEventos
+        }}
+      >
+        {this.props.children}
+      </EventosContext.Provider>
+    );
+  }
+}
+
+export default EventosProvider;
