782f2258228585239df059efa9153c86929a08f6
Erick <dev.llerenas@gmail.com>
2020-03-25 22:24:03 -0600

    removed dog

---
+++ DOG API ANTD/src/App.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import { Card, Cascader, Row, Col } from "antd";
+import "antd/dist/antd.css";
+const { Meta } = Card;
+
+function App() {
+  const options = [
+    {
+      value: "husky",
+      label: "Husky"
+    },
+    {
+      value: "boxer",
+      label: "Boxer"
+    },
+    {
+      value: 'beagle',
+      label: 'Beagle'
+    },
+    {
+      value: 'chihuahua',
+      label: 'Chihuahua'
+    }
+  ];
+
+  function onChange(value) {
+    setDogType(value[0]);
+  }
+
+  const [dogs, setDogs] = useState([]);
+  const [dogType, setDogType] = useState("beagle");
+
+  useEffect(() => {
+    const obtenerDatosDeLaAPI = async () => {
+      const result = await fetch(`https://dog.ceo/api/breed/${dogType}/images`);
+  
+      const json = await result.json();
+      setDogs(json.message);
+    };
+    obtenerDatosDeLaAPI();
+  }, [dogType]);
+
+  return (
+    <div>
+      <Cascader options={options} onChange={onChange} />
+      <Row>
+        {dogs.map((dog, index) => (
+          <Col xs={24} md={12} lg={8} key={index}>
+            <Card
+              hoverable
+              style={{margin:10}}
+              cover={<img alt="dog" src={dog} style={{height:300}}/>}
+            >
+              <Meta title={dogType.toUpperCase()} />
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+}
+
+export default App;
